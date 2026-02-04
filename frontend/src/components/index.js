fc653c3b321e3b3ed95e70c473e73239efb3b198
Saurabh Kashyap <kashyap.sau@northeastern.edu>
2026-02-04 02:45:07 -0500

    added barrel export for components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+// barrel export for components
+export { default as Header } from './Header'
+export { default as ParticipantsList } from './ParticipantsList'
+export { ToastProvider, useToast } from './Toast'
